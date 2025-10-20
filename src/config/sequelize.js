453a6d609e73b6ac51f47ce67a7b81a5bf5f670d
duduferro <carlos.ferro@aluno.senai.br>
2025-10-20 07:56:37 -0300

    teste

--- src/config/sequelize.js
+++
@@ -1,6 +0,0 @@
-import { Sequelize } from "sequelize"
-
-export const conn = new Sequelize({
-    dialect: "sqlite",
-    storage: "./autoTruck.sqlite"
-})
--- src/feature/usuarios/usuariosRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { criarUsuario } from "./usuariosController.js";
-
-const router = Router()
-
-router.post("/", criarUsuario)
-
-export default router
